f00f42dd2b34b7d98d56fa804dd24c00004f41fb
Nikita Kitaev <nikitakit@gmail.com>
2011-10-15 01:50:13 -0700

    Fix how simulator deals with the deck

--- simulator.py
+++ simulator.py
@@ -9,13 +9,22 @@ class Simulator:
         self.p0=p0
         self.p1=p1
 
+    def deal(self):
+        if len(self.deck) == 0:
+            print("RESHUFFLING DECK")
+            self.deck=self.buried
+            self.buried=[]
+
+        return self.deck.pop()
+
     def run_game(self):
         deal=list(range(81)[1:])
         random.shuffle(deal)
         self.p0_hand=deal[0:20]
         self.p1_hand=deal[20:40]
         self.discard=deal[40]
-        deck=deal[41:]
+        self.buried=[]
+        self.deck=deal[41:]
 
         self.p0.start_game(1,0,self.discard,1)
         self.p1.start_game(1,1,self.discard,0)
@@ -29,11 +38,11 @@ class Simulator:
                 self.p0_hand[i]=self.discard
                 self.discard=tmp
             elif m0["move"]=="request_deck":
-                d=deck.pop()
+                d=self.deal()
                 i=self.p0.get_deck_exchange(1,1000,self.p0_hand,d)
-                tmp=self.p0_hand[i]
-                self.p0_hand[i]=self.discard
-                self.discard=tmp
+                self.buried.append(self.discard)
+                self.discard=self.p0_hand[i]
+                self.p0_hand[i]=d
             else:
                 print ("Invalid move", m0)
                 assert False
@@ -49,11 +58,11 @@ class Simulator:
                 self.p1_hand[i]=self.discard
                 self.discard=tmp
             elif m1["move"]=="request_deck":
-                d=deck.pop()
+                d=self.deal()
                 i=self.p1.get_deck_exchange(1,1000,self.p1_hand,d)
-                tmp=self.p1_hand[i]
-                self.p1_hand[i]=self.discard
-                self.discard=tmp
+                self.buried.append(self.discard)
+                self.discard=self.p1_hand[i]
+                self.p1_hand[i]=d
             else:
                 print ("Invalid move", m0)
                 assert False
